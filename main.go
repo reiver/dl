6d1b8e4985cec303fbe8af0bf7c210fea0e999ac
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-30 15:12:03 -0700

    initial commits

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	}
 
 	// If the user asked for the help message to be outputted (by calling this program with the --help flag) then
-	// output the help message (on STDERR), and exit with an exit code of ‘OK’ — i.e., 0 (zero).
+	// output the help message (on STDERR), and exit with an exit-code of ‘OK’ — i.e., 0 (zero).
 	if arg.Help {
 		const exitCodeOK = 0
 
@@ -33,7 +33,7 @@ func main() {
 	}
 
 	// If the user didn't provide any flags, switches, or parameters, on the command line then
-	// output the help message (on STDERR), and exit with an exit code of ‘usage-error’ — i.e., 64 (sixty-four).
+	// output the help message (on STDERR), and exit with an exit-code of ‘usage-error’ — i.e., 64 (sixty-four).
 	if optstr.Nothing() == arg.Target {
 		const exitCodeUsageError = 64
 
@@ -46,7 +46,7 @@ func main() {
 	// Also, the user can override the ‘scheme’ and ‘host’ (inferred from the ‘target’).
 	//
 	// Thus, if for some weird reason we have the ‘target’ but don't have the ‘scheme’ or ‘host’ then
-	// some weird internal error (like a bug) happened so exit with an exit code of ‘internal-software-error’ — i.e., 70 (seventy).
+	// some weird internal error (like a bug) happened so exit with an exit-code of ‘internal-software-error’ — i.e., 70 (seventy).
 	if optstr.Nothing() == arg.Scheme {
 		const exitCodeInternalSoftwareError = 70
 
