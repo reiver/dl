8edecc515f1c7b3db4126efd2d58ba28563aa819
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-30 11:20:27 -0700

    initial commits

---
+++ lib/help/writeto.go
@@ -0,0 +1,53 @@
+package help
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"strings"
+	"unicode/utf8"
+)
+
+func WriteTo(w io.Writer) (int64, error) {
+
+	var msg strings.Builder
+
+	fmt.Fprint(&msg,
+		"dl — a free utility for non-interactive downloading of files and streams"              +"\n\n"+
+		"usage:"                                                                                +"\n",
+		"\tdl [options] [target]"                                                               +"\n\n"+
+		"examples:"                                                                             +"\n"+
+		"\tdl gemini://example.com/apple/banana/cherry"                                         +"\n"+
+		"\tdl http://example.com/path/to/file.txt"                                              +"\n\n"+
+		"\tdl mercury://example.com/path/to/file.txt"                                           +"\n\n"+
+		"\tdl --target='gemini://example.com/apple/banana/cherry'"                              +"\n"+
+		"\tdl --target='http://example.com/path/to/file.txt'"                                   +"\n\n"+
+		"\tdl --host='cache.something.com' gemini://example.com/apple/banana/cherry"            +"\n\n"+
+		"\tdl --host='cache.something.com' --target='gemini://example.com/apple/banana/cherry'" +"\n\n"+
+		"flags:"                                                                                +"\n",
+	)
+	flag.VisitAll(func(fl *flag.Flag){
+		switch fl.Name {
+		case "v", "vv", "vvv", "vvvv", "vvvvv", "vvvvvv":
+			fmt.Fprint(&msg, " -")
+		default:
+			switch {
+			case 2 > utf8.RuneCountInString(fl.Name):
+				fmt.Fprint(&msg, " -")
+			default:
+				fmt.Fprint(&msg, " --")
+			}
+		}
+		fmt.Fprintf(&msg, "%s\n\t%s\n\n", fl.Name, fl.Usage)
+
+	})
+
+
+	{
+		n, err := io.WriteString(w, msg.String())
+
+		var n64 int64 = int64(n)
+
+		return n64, err
+	}
+}
